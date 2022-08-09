92e62bb7a56ba34ff67765bb57a5caffbdd354d7
Türhan Yıldırım <yildirimturhan@gmail.com>
2022-08-09 09:50:00 +0300

    Update UserViewModel.cs

--- src/PriceHunter.Contract/App/User/UserViewModel.cs
+++ src/PriceHunter.Contract/App/User/UserViewModel.cs
@@ -5,5 +5,7 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Image { get; set; }
+        public string Email { get; set; }
+        public string Type { get; set; }
     }
 }
