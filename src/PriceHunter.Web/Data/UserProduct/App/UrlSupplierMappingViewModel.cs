d5baa571fba0bbd8a172620b02bcf2786727b693
Türhan Yıldırım <yildirimturhan@gmail.com>
2022-08-25 14:54:27 +0300

    Back button added.

--- src/PriceHunter.Web/Data/UserProduct/App/UrlSupplierMappingViewModel.cs
+++ src/PriceHunter.Web/Data/UserProduct/App/UrlSupplierMappingViewModel.cs
@@ -5,6 +5,7 @@ namespace PriceHunter.Web.Data.UserProduct.App
     public class UrlSupplierMappingViewModel
     {
         [Url]
+        [MaxLength(1000)]
         public string Url { get; set; }
         public string Supplier { get; set; }
         public Guid SupplierId { get; set; }
--- src/PriceHunter.Web/Helpers/Constants/NavigationConstants.cs
+++ src/PriceHunter.Web/Helpers/Constants/NavigationConstants.cs
@@ -6,5 +6,11 @@
         public static readonly string Logout = "/Logout";
 
         public static readonly string UserProfile = "/User/Profile";
+
+        public static readonly string UserProductAdd = "/User/Products/Add";
+        public static readonly string UserProductEdit = "/User/Products/Edit/";
+        public static readonly string UserProductDetail = "/User/Products/Detail/";
+        public static readonly string UserProductList = "/User/Products/List";
+        
     }
 }
